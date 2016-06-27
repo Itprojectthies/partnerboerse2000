95bae25d670a614c069a24648a0e700a249682ee
DanielVolz <volz.daniel@gmail.com>
2016-06-27 21:19:48 +0200

    BasicFrame überarbeitet

--- superteam2000/src/de/superteam2000/gwt/client/BasicFrame.java
+++ superteam2000/src/de/superteam2000/gwt/client/BasicFrame.java
@@ -6,41 +6,32 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * Diese Klasse ist die Basisklasse aller BasicFrames. Jeder BasicFrame ist ein VerticalPanel und
- * lässt sich somit unter GWT entsprechend anordnen.
+ * Diese Klasse ist die Basisklasse aller BasicFrames. Jeder BasicFrame ist ein FlowPanel
  *
- * @author thies
+ * @author Thies, Volz
  * @version 1.0
  *
  */
 public abstract class BasicFrame extends FlowPanel {
 
-  /**
-   * Jedes GWT Widget muss diese Methode implementieren. Sie gibt an, sas geschehen soll, wenn eine
-   * Widget-Instanz zur Anzeige gebracht wird.
-   */
+
   @Override
   public void onLoad() {
     /*
-     * Bevor wir unsere eigene Formatierung veranslassen, überlassen wir es der Superklasse eine
-     * Initialisierung vorzunehmen.
+     * Die Super-Klasse initialisiert das Widget
      */
 
     super.onLoad();
+    
+    /*
+     * Füge die jeweilige Implementation der Headline und der Subheadline zum FlowPanel hinzu
+     */
     this.add(createHeadline(getHeadlineText(), getSubHeadlineText()));
+    /*
+     * Lösche die Datagrid für jede konkretisierte Klasse 
+     */
     RootPanel.get("search-table").clear();
-    /*
-     * Als erstes geben wir stets die Headline des BasicFrame aus. Da getHeadlineText() als
-     * abstrakte Methode bzw. als Einschubmethode realisiert wird, obliegt es den Subklassen, für
-     * eine Ausgestaltung also Implementierung zu sorgen.
-     */
-
-
-    /*
-     * Wenn alles vorbereitet ist, stoßen wir die run()-Methode an. Auch run() ist als abstrakte
-     * Methode bzw. als Einschubmethode realisiert. Auch hier ist es Aufgabe der Subklassen, für
-     * deren Implementierung zu sorgen.
-     */
+   
     run();
 
   }
@@ -62,38 +53,17 @@ public abstract class BasicFrame extends FlowPanel {
 
 
   /**
-   * Mit Hilfe dieser Methode erstellen wir aus einem Strinng ein mittels CSS formatierbares
-   * HTML-Element, das an das Ende der bisherigen Ausgabe dieses BasicFrame angehängt wird. Unter
-   * CSS lässt sich das Ergebnis über <code>.bankproject-simpletext</code> referenzieren bzw.
-   * formatieren.
-   *
-   * @param text der String, den wir als HTML an die bisherige BasicFrame-Ausgabe anhängen wollen.
-   */
-  protected void append(String text) {
-    HTML content = new HTML(text);
-    // content.setStylePrimaryName("bankproject-simpletext");
-    this.add(content);
-  }
-
-  /**
-   * Mit Hilfe dieser Methode erstellen wir aus einem Strinng ein mittels CSS formatierbares
-   * HTML-Element, das an das Ende der bisherigen Ausgabe dieses BasicFrame angehängt wird. Unter
-   * CSS lässt sich das Ergebnis über <code>.bankproject-simpletext</code> referenzieren bzw.
-   * formatieren.
-   *
-   * @param text der String, den wir als HTML an die bisherige BasicFrame-Ausgabe anhängen wollen.
-   */
-  protected void append(Widget wi) {
-    this.add(wi);
-  }
-
-  /**
-   * Abstrakte Einschubmethode, die in den Subklassen zu realisieren ist.
+   * Abstrakte Einschubmethode, die die Überschrift setzt.
    *
    * @return der Text, den wir als Headline setzen wollen.
    */
   protected abstract String getHeadlineText();
-
+  
+  /**
+  * Abstrakte Einschubmethode, die die Sub-Überschrift setzt
+  *
+  * @return der Text, den wir als Headline setzen wollen.
+  */
   protected abstract String getSubHeadlineText();
 
   /**
