242b78641bcb27e7f428a462ce8d38aa7be744c9
DanielVolz <volz.daniel@gmail.com>
2016-06-27 21:55:25 +0200

    Merkliste überarbeitet

--- superteam2000/src/de/superteam2000/gwt/client/Merkliste.java
+++ superteam2000/src/de/superteam2000/gwt/client/Merkliste.java
@@ -16,14 +16,19 @@ import de.superteam2000.gwt.shared.bo.Profil;
 /**
  * Diese Klasse ist zum Anzeigen sämtlicher vom Benutzer gemerkten Profile.
  *
- * @author Christopher
+ * @author Christopher Funke, Volz Daniel
  *
  */
 public class Merkliste extends BasicFrame {
+  /*
+   * Alle notwendigen Instanzvariablen werden deklariert
+   */
 
-  // pb Verwaltung über ClientsideSettings holen
   PartnerboerseAdministrationAsync pbVerwaltung = ClientsideSettings.getPartnerboerseVerwaltung();
-  Profil profil = ClientsideSettings.getCurrentUser();
+
+  Profil user = ClientsideSettings.getCurrentUser();
+
+  ArrayList<Profil> profile = new ArrayList<Profil>();
 
   @Override
   public String getHeadlineText() {
@@ -31,86 +36,32 @@ public class Merkliste extends BasicFrame {
     return "Merkliste";
   }
 
-  // Das ausgewählte Profil
-  private Profil selected = null;
-
-  ArrayList<Profil> profile = new ArrayList<Profil>();
+  @Override
+  protected String getSubHeadlineText() {
+    return "Hier findest du deine gemerkten Profile";
+  }
 
   @Override
   public void run() {
 
     // Merkliste abfragen und anzeigen
-    pbVerwaltung.getMerkzettelForProfil(profil, new AsyncCallback<Merkzettel>() {
-
-      @Override
-      public void onSuccess(Merkzettel result) {
-
-        profile = result.getGemerkteProfile();
-        DataGridProfiles dgp = new DataGridProfiles(profile);
-        dgp.addClickFremdProfil();
-
-        RootPanel.get("main").add(dgp.start());
-
-      }
-
-      @Override
-      public void onFailure(Throwable caught) {
-
-        ClientsideSettings.getLogger().info("Fehler AsyncCallback Merkzettel in Merkliste");
-
-      }
-    });
+    pbVerwaltung.getMerkzettelForProfil(user, new MerkzettelForProfilCallback());
 
   }
 
-  /**
-   * Clickhandler für den entfernenButton ausgewähltes Element wird von der Liste entfernt (auch aus
-   * db)
-   *
-   * @author Christopher
-   *
-   */
-  public class EntfernenButtonClickhandler implements ClickHandler {
+  private class MerkzettelForProfilCallback implements AsyncCallback<Merkzettel> {
     @Override
-    public void onClick(ClickEvent event) {
-      if (selected != null) {
-        ClientsideSettings.getPartnerboerseVerwaltung()
-            .deleteMerken(ClientsideSettings.getCurrentUser(), selected, new AsyncCallback<Void>() {
+    public void onSuccess(Merkzettel result) {
+      profile = result.getGemerkteProfile();
+      DataGridProfiles dgp = new DataGridProfiles(profile);
+      dgp.addClickFremdProfil();
 
-              @Override
-              public void onSuccess(Void result) {
-                RootPanel.get("main").clear();
-                Merkliste m = new Merkliste();
-                RootPanel.get("main").add(m);
+      RootPanel.get("main").add(dgp.start());
+    }
 
-                Window.alert("Profil wurde von der Merkliste entfernt!");
-
-              }
-
-              @Override
-              public void onFailure(Throwable caught) {
-                // TODO Auto-generated method stub
-
-              }
-            });
-
-      }
-
+    @Override
+    public void onFailure(Throwable caught) {
+      ClientsideSettings.getLogger().info("Fehler AsyncCallback Merkzettel in Merkliste");
     }
   }
-
-  /**
-   * Clickhandler der das ausgewählte Profil anzeigt, und es als besucht markiert
-   *
-   * @author Christopher
-   *
-   */
-
-
-  @Override
-  protected String getSubHeadlineText() {
-    // TODO Auto-generated method stub
-    return "Hier findest du deine gemerkten Profile";
-  }
-
 }
