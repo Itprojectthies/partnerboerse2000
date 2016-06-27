d6500df6159659bb1d923952a8682e54ad53ebc8
DanielVolz <volz.daniel@gmail.com>
2016-06-27 21:40:31 +0200

    CreateProfil überarbeitet

--- superteam2000/src/de/superteam2000/gwt/client/CreateProfil.java
+++ superteam2000/src/de/superteam2000/gwt/client/CreateProfil.java
@@ -24,25 +24,27 @@ import de.superteam2000.gwt.shared.bo.Beschreibung;
 import de.superteam2000.gwt.shared.bo.Profil;
 
 /**
- * Formular für die Darstellung des selektierten Kunden
+ * Formular zum Erstellen eines Nutzers
  *
- * @author Christian Rathke
+ * @author Christian Rathke, Volz Daniel
  */
 public class CreateProfil extends BasicFrame {
 
   PartnerboerseAdministrationAsync pbVerwaltung = ClientsideSettings.getPartnerboerseVerwaltung();
 
   /*
-   * Widgets, deren Inhalte variable sind, werden als Attribute angelegt.
+   * Alle notwendigen Instanzvariablen werden deklariert
    */
 
   Profil user = ClientsideSettings.getCurrentUser();
   Logger logger = ClientsideSettings.getLogger();
 
-  FlowPanel fPanel = new FlowPanel();
-  FlowPanel fPanel2 = new FlowPanel();
+  FlowPanel contentPanel = new FlowPanel();
+  FlowPanel alignPanel = new FlowPanel();
+
   ProfilAttributListbox gebTag = null;
   ProfilAttributListbox groesse = null;
+
   Button confirmBtn = null;
 
   @Override
@@ -50,15 +52,17 @@ public class CreateProfil extends BasicFrame {
     return "Profil erstellen";
   }
 
-  /*
-   * Im Konstruktor werden die anderen Widgets erzeugt. Alle werden in einem Raster angeordnet,
-   * dessen Größe sich aus dem Platzbedarf der enthaltenen Widgets bestimmt.
-   */
+  @Override
+  protected String getSubHeadlineText() {
+    // TODO Auto-generated method stub
+    return "Erstelle dein Profil und lege gleich los!";
+  }
 
   @Override
   public void run() {
-    fPanel.setStyleName("pure-form pure-form-aligned");
-    fPanel2.setStyleName("content");
+    contentPanel.setStyleName("content");
+    alignPanel.setStyleName("pure-form pure-form-aligned");
+
     gebTag = new ProfilAttributListbox();
     gebTag.createGebtaListobx("Was ist dein Geburtstag");
     gebTag.setEnable(true);
@@ -67,16 +71,17 @@ public class CreateProfil extends BasicFrame {
     groesse.createGroesseListBox("Was ist deine Körpergröße");
     groesse.setEnable(true);
 
-    pbVerwaltung.getAllBeschreibungProfilAttribute(new GetAllBeschreibungProfilAttributeCallBack());
-    pbVerwaltung.getAllAuswahlProfilAttribute(new GetAllAuswahlProfilAttributeCallBack());
-
-
     confirmBtn = new Button("Weiter");
     confirmBtn.setStyleName("pure-button pure-button-primary");
 
     confirmBtn.addClickHandler(new ConfirmClickHandler());
-    fPanel2.add(fPanel);
-    RootPanel.get("main").add(fPanel2);
+
+    pbVerwaltung.getAllBeschreibungProfilAttribute(new GetAllBeschreibungProfilAttributeCallBack());
+    pbVerwaltung.getAllAuswahlProfilAttribute(new GetAllAuswahlProfilAttributeCallBack());
+
+    contentPanel.add(alignPanel);
+
+    RootPanel.get("main").add(contentPanel);
 
   }
 
@@ -88,15 +93,12 @@ public class CreateProfil extends BasicFrame {
       for (Beschreibung b : result) {
 
         BoxPanel clb = new BoxPanel(b, false);
-        fPanel.add(clb);
+        alignPanel.add(clb);
       }
-
     }
 
     @Override
-    public void onFailure(Throwable caught) {
-
-    }
+    public void onFailure(Throwable caught) {}
   }
 
   private class GetAllAuswahlProfilAttributeCallBack implements AsyncCallback<ArrayList<Auswahl>> {
@@ -104,115 +106,24 @@ public class CreateProfil extends BasicFrame {
     @Override
     public void onSuccess(ArrayList<Auswahl> result) {
       for (Auswahl a : result) {
-
         BoxPanel clb = new BoxPanel(a, false);
-        fPanel.add(clb);
+        alignPanel.add(clb);
       }
 
-      fPanel.add(groesse);
-      fPanel.add(gebTag);
-      fPanel.add(confirmBtn);
+      alignPanel.add(groesse);
+      alignPanel.add(gebTag);
+      alignPanel.add(confirmBtn);
     }
 
     @Override
-    public void onFailure(Throwable caught) {
-
-    }
+    public void onFailure(Throwable caught) {}
   }
 
   private class ConfirmClickHandler implements ClickHandler {
 
     @Override
     public void onClick(ClickEvent event) {
-
-      String firstName = "";
-      String lastName = "";
-
-      String haarfarbe = "";
-      String raucher = "";
-      String religion = "";
-      String geschlecht = "";
-      String email = user.getEmail();
-
-      int groesse = 141;
-
-      int geburtsTag = 2;
-      int geburtsMonat = 2;
-      int geburtsJahr = 1901;
-
-      // Schleifen zum Auslesen der Listboxen, welche in 2 Panels
-      // verschachtelt sind
-
-      for (Widget child : fPanel) {
-        if (child instanceof FlowPanel) {
-          FlowPanel childPanel = (FlowPanel) child;
-          for (Widget box : childPanel) {
-            if (box instanceof EigenschaftListBox) {
-              EigenschaftListBox lb = (EigenschaftListBox) box;
-              logger.severe("test " + lb.getName());
-
-              switch (lb.getName()) {
-
-                case "Raucher":
-                  raucher = lb.getSelectedItemText();
-                  break;
-                case "Haarfarbe":
-                  haarfarbe = lb.getSelectedItemText();
-                  break;
-                case "Religion":
-                  religion = lb.getSelectedItemText();
-                  break;
-                case "Geschlecht":
-                  geschlecht = lb.getSelectedItemText();
-                  break;
-                case "Körpergröße":
-                  groesse = Integer.valueOf(lb.getSelectedItemText());
-                  break;
-                case "GeburtstagTag":
-                  geburtsTag = Integer.valueOf(lb.getSelectedItemText());
-                  break;
-                case "GeburtstagMonat":
-                  geburtsMonat = Integer.valueOf(lb.getSelectedItemText());
-                  break;
-                case "GeburtstagJahr":
-                  geburtsJahr = Integer.valueOf(lb.getSelectedItemText());
-                  break;
-
-              }
-
-            } else if (box instanceof TextBox) {
-              TextBox tb = (TextBox) box;
-              logger.severe("test " + tb.getName());
-              switch (tb.getName()) {
-                case "Vorname":
-                  firstName = tb.getText();
-                  break;
-                case "Nachname":
-                  lastName = tb.getText();
-                  break;
-
-              }
-            }
-
-          }
-        }
-
-      }
-
-      // Date-Objekt aus den 3 Geburtstagswerten Tag, Monat und Jahr
-      // konstruieren und in
-      // ein SQL-Date-Objekt umwandeln
-
-      Date gebTag2 = DateTimeFormat.getFormat("yyyy-MM-dd")
-          .parse(geburtsJahr + "-" + geburtsMonat + "-" + geburtsTag);
-      java.sql.Date gebTag = new java.sql.Date(gebTag2.getTime());
-
-      if (!firstName.isEmpty() && !lastName.isEmpty()) {
-        pbVerwaltung.createProfil(lastName, firstName, email, gebTag, haarfarbe, raucher, religion,
-            groesse, geschlecht, new CreateCustomerCallback());
-      } else {
-        new Notification("Bitte füllen sie alle Felder aus", "warning");
-      }
+      createProfil();
 
     }
   }
@@ -228,8 +139,10 @@ public class CreateProfil extends BasicFrame {
     @Override
     public void onSuccess(Profil p) {
       p.setLogoutUrl(user.getLogoutUrl());
-      ClientsideSettings.setCurrentUser(p);
       p.setLoggedIn(true);
+      
+      ClientsideSettings.setCurrentUser(p);
+      
       ShowProfil sp = new ShowProfil();
       Navbar nb = new Navbar();
       RootPanel.get("main").clear();
@@ -239,10 +152,92 @@ public class CreateProfil extends BasicFrame {
 
   }
 
-  @Override
-  protected String getSubHeadlineText() {
-    // TODO Auto-generated method stub
-    return "Erstelle dein Profil und lege gleich los!";
+
+
+  private void createProfil() {
+    String firstName = "";
+    String lastName = "";
+
+    String haarfarbe = "";
+    String raucher = "";
+    String religion = "";
+    String geschlecht = "";
+    String email = user.getEmail();
+
+    int groesse = 141;
+
+    int geburtsTag = 2;
+    int geburtsMonat = 2;
+    int geburtsJahr = 1901;
+
+    // Schleifen zum Auslesen der Listboxen, welche in 2 Panels
+    // verschachtelt sind
+
+    for (Widget child : alignPanel) {
+      if (child instanceof EigenschaftListBox) {
+        EigenschaftListBox lb = (EigenschaftListBox) child;
+        logger.severe("test " + lb.getName());
+
+        switch (lb.getName()) {
+
+          case "Raucher":
+            raucher = lb.getSelectedItemText();
+            break;
+          case "Haarfarbe":
+            haarfarbe = lb.getSelectedItemText();
+            break;
+          case "Religion":
+            religion = lb.getSelectedItemText();
+            break;
+          case "Geschlecht":
+            geschlecht = lb.getSelectedItemText();
+            break;
+          case "Körpergröße":
+            groesse = Integer.valueOf(lb.getSelectedItemText());
+            break;
+          case "GeburtstagTag":
+            geburtsTag = Integer.valueOf(lb.getSelectedItemText());
+            break;
+          case "GeburtstagMonat":
+            geburtsMonat = Integer.valueOf(lb.getSelectedItemText());
+            break;
+          case "GeburtstagJahr":
+            geburtsJahr = Integer.valueOf(lb.getSelectedItemText());
+            break;
+
+        }
+
+      } else if (child instanceof TextBox) {
+        TextBox tb = (TextBox) child;
+        logger.severe("test " + tb.getName());
+        switch (tb.getName()) {
+          case "Vorname":
+            firstName = tb.getText();
+            break;
+          case "Nachname":
+            lastName = tb.getText();
+            break;
+
+        }
+      }
+
+
+    }
+
+    // Date-Objekt aus den 3 Geburtstagswerten Tag, Monat und Jahr
+    // konstruieren und in
+    // ein SQL-Date-Objekt umwandeln
+
+    Date gebTag2 = DateTimeFormat.getFormat("yyyy-MM-dd")
+        .parse(geburtsJahr + "-" + geburtsMonat + "-" + geburtsTag);
+    java.sql.Date gebTag = new java.sql.Date(gebTag2.getTime());
+
+    if (!firstName.isEmpty() && !lastName.isEmpty()) {
+      pbVerwaltung.createProfil(lastName, firstName, email, gebTag, haarfarbe, raucher, religion,
+          groesse, geschlecht, new CreateCustomerCallback());
+    } else {
+      new Notification("Bitte füllen sie alle Felder aus", "warning");
+    }
   }
 
 }
